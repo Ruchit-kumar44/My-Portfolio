f4a201dabb686bc54a7453f013e36a053d76d1e5
Ruchit kumar <ruchitk539@gmail.com>
2026-09-24 11:17:03 +0530

    edit landing project section

--- src/components/landing/Projects.tsx
+++ src/components/landing/Projects.tsx
@@ -1,7 +1,19 @@
+import { projects } from "@/config/Projects";
+import Container from "../common/Container";
+import ProjectList from "../projects/ProjectList";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function ProjectsSection(){
     return(
-        <div className="w-full min-h-50 bg-gray-black">
-        </div>
+       <Container className="mt-20">
+         <h1>Projects</h1>
+         <ProjectList className="" projects={projects.slice(0, 2)} />
+          <div className="mt-5 flex justify-center">
+            <Button variant="outline" >
+                <Link href='/projects'>Show all projects</Link>
+            </Button>
+          </div>
+       </Container>
     )
 }
