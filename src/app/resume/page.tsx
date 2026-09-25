b140e9e49d82e0f15e6d3bca6d768b9f8d391b9e
Ruchit kumar <ruchitk539@gmail.com>
2026-09-25 11:49:52 +0530

    add resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/common/Container";
+import { Separator } from "@/components/ui/separator";
 import { generateMetadata as getMetadata } from "@/config/Meta";
 import { Metadata } from "next";
 
@@ -19,8 +20,20 @@ export const metadata: Metadata = {
 
 export default function ResumePage() {
   return (
-    <Container className="py-16">
-      <div>this is resume page</div>
+    <Container className="py-12">
+        <div className="space-y-12">
+          <div>
+            <h1 className="font-bold tracking-tight text-2xl">Resume</h1>
+            <p className="text-secondary font text-[16px]">View and download my professional Resume</p>
+          </div>
+          <Separator />
+          <div className="max-full">
+            <iframe 
+            src="https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview" 
+            className="w-full min-h-screen"
+            ></iframe>
+          </div>
+        </div>
     </Container>
   );
 }
